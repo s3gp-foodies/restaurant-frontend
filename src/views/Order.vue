eb24de907641b1185264810a28de3a16a81ccead
Frenske99 <39583221+Frenske99@users.noreply.github.com>
2022-03-14 14:16:59 +0100

    Textbox with + and - with limitations

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,11 +2,39 @@
   <div class="order">
     <h2>Order Page</h2>
   </div>
+  <p>Menu-item</p>
+  <input v-model="testing" class="form-control" type="number" placeholder={testing} @input="OnInput">
+  <button type="button" class="btn btn-success" @click="add">+</button>
+  <button type="button" class="btn btn-danger" @click="del">-</button>
+  <div v-if="this.testing >9">Cannot be more then 10</div>
 </template> 
 
 <script>
 export default {
-  name: 'OrderPage'
+  name: 'OrderPage',
+  
+  methods: {
+      add : function () {
+          this.testing ++,
+          this.OnInput()
+      },
+      del : function (){
+          this.testing --,
+          this.OnInput()
+      },
+      OnInput: function(){
+        if(this.testing  <0){
+          this.testing = 0
+        }else if (this.testing >= 10){
+          this.testing = 10
+        }
+      }
+  },
+  data() {
+    return {
+      testing:0
+    }
+  }
 }
 </script>
 
