4c36f8865ff0f88b53d964e7eb74e24c19abaf6c
juju124 <justin555@live.nl>
2022-03-15 12:00:52 +0100

    Added menu loading from json

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,175 +1,34 @@
 <template>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <div class="divider">
-    <p class="dividerBarTxt"> Voorgerechten </p>
+  <div v-for="menuItems in data" :key="menuItems"  class="divider">
+    <p class="dividerBarTxt"> {{Object.keys((menuItems))[0]}} </p>
   </div>
 
+
+
   <!-- Styled Menu Item with Bootstrap -->
-  <div class="card text-black bg-light mb-3 menuItem">
-    <h3 class="display-6 card-header">Zalm rolletjes</h3>
+  <div v-for="(dishes) in data['MenuList']" :key="dishes">
+    <div v-for="(dish, key) in dishes" :key="dish" class="card text-black bg-light mb-3 menuItem">
+    <h3 class="display-6 card-header">{{key}}</h3>
     <div class="card-body text-black">
-    <img class="foodImage" src="img/zalmRolletjes.jpg" alt="">
-    <p class="foodTxt">
-      Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
+    <img class="foodImage" :src="dish.imgLink" alt="">
+    <p class="foodTxt"> {{dish.description}} </p>
     <p class="priceTxT">
       <button type="button" class="btn btn-outline-secondary"><i class="fa fa-minus"></i></button>
       <input style="height: 30px;" type="number" class="numberInput" />
       <button type="button" class="btn btn-outline-secondary"><i class="fa fa-plus"></i></button>
       <button  type="submit" class="btn btn-outline-secondary"> Voeg toe </button> <br>
-      &euro; 4,50 Per stuk  </p>
+      &euro; {{dish.price}} Per stuk  </p>
+    </div>
     </div>
   </div>
 
-  <div class="card text-black bg-light mb-3 menuItem">
-    <h3 class="display-6 card-header"> Schaal met Zalmtoastjes  </h3>
-    <div class="card-body text-black">
-    <img class="foodImage" src="img/zalmToast.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button type="button" class="btn btn-outline-secondary"><i class="fa fa-minus"></i></button>
-      <input  style="height: 30px;"  type="number" class="numberInput">
-      <button type="button" class="btn btn-outline-secondary"><i class="fa fa-plus"></i></button>
-      <button type="submit" class="btn btn-outline-secondary"> Voeg toe </button> &euro; 1,50 Per stuk  </p>
-    </div>
+  <div v-for="menuItems in data" :key="menuItems"  class="divider">
+    <p class="dividerBarTxt"> {{Object.keys((menuItems))[1]}} </p>
   </div>
 
-
-  <div class="menuItem">
-    <h3> Fruit Salade  </h3>
-    <img class="foodImage" src="img/fruitsalade.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button><i class="fa fa-minus"></i></button>
-      <input type="number" class="numberInput">
-      <button><i class="fa fa-plus"></i></button>
-      <button> Voeg toe </button>
-      &euro; 1,50 Per stuk </p>
-  </div>
-
-
-  <div class="menuItem">
-    <h3> Gevulde Toast </h3>
-    <img class="foodImage" src="img/gevuldeToast.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button><i class="fa fa-minus"></i></button>
-      <input type="number" class="numberInput">
-      <button><i class="fa fa-plus"></i></button>
-      <button> Voeg toe </button> &euro; 2,50 Per stuk </p>
-  </div>
-
-
-  <div class="divider">
-    <p class="dividerBarTxt"> Hoofdgerechten </p>
-  </div>
-  <div class="menuItem">
-    <h3> Pasta </h3>
-    <img class="foodImage" src="img/Pasta.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button><i class="fa fa-minus"></i></button>
-      <input type="number" class="numberInput">
-      <button><i class="fa fa-plus"></i></button>
-      <button> Voeg toe </button> &euro; 10,50 Per stuk </p>
-  </div>
-
-
-  <div class="menuItem">
-    <h3> Burger Menu </h3>
-    <img class="foodImage" src="img/burger.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button><i class="fa fa-minus"></i></button>
-      <input type="number" class="numberInput">
-      <button><i class="fa fa-plus"></i></button>
-      <button> Voeg toe </button> &euro; 15,50 Per stuk </p>
-  </div>
-
-  <div class="menuItem">
-    <h3> Ham en een Stampot schotel </h3>
-    <img class="foodImage" src="img/ham.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button><i class="fa fa-minus"></i></button>
-      <input type="number" class="numberInput">
-      <button><i class="fa fa-plus"></i></button>
-      <button> Voeg toe </button> &euro; 15,50 Per stuk </p>
-  </div>
-
-  <div class="menuItem">
-    <h3> T-bone steak </h3>
-    <img class="foodImage" src="img/hoofdgerecht.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button><i class="fa fa-minus"></i></button>
-      <input type="number" class="numberInput">
-      <button><i class="fa fa-plus"></i></button>
-      <button> Voeg toe </button> &euro; 25,50 Per stuk </p>
-  </div>
-
-  <div class="divider">
-    <p class="dividerBarTxt"> Nagerechten </p>
-  </div>
-  <div class="menuItem">
-    <h3> Speculaase themed special  </h3>
-    <img class="foodImage" src="img/speculaas.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button><i class="fa fa-minus"></i></button>
-      <input type="number" class="numberInput">
-      <button><i class="fa fa-plus"></i></button>
-      <button> Voeg toe </button> &euro; 10,50 Per stuk </p>
-  </div>
-
-  <div class="menuItem">
-    <h3> Fruity afternoon </h3>
-    <img class="foodImage" src="img/strawberry.jpg" alt="">
-    <p class="foodTxt">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-      toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken.
-      Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting.
-      Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software
-      zoals Aldus PageMaker die versies van Lorem Ipsum bevatten</p>
-    <p class="priceTxT">
-      <button><i class="fa fa-minus"></i></button>
-      <input type="number" class="numberInput">
-      <button><i class="fa fa-plus"></i></button>
-      <button> Voeg toe </button> &euro; 7,50 Per stuk </p>
+  <div v-for="menuItems in data" :key="menuItems"  class="divider">
+    <p class="dividerBarTxt"> {{Object.keys((menuItems))[2]}} </p>
   </div>
 </template>
 
