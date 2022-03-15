abc158dd71f7b16dc5d26ccae2e5b5ce7dd4e438
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-03-15 10:58:20 +0100

    Changed json orders

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -5,7 +5,7 @@
       <tr v-for="order in orders['order']" :key="order">
         <td>{{ order.name }}</td>
         <td>{{ order.amount }} x {{ order.price }}</td>
-        <td>{{ multiplyPriceTotals(order.amount, order.price) }}</td>
+        <td>{{ multiplyPriceTotalOrders(order.amount, order.price) }}</td>
       </tr>
       <tr>
         <th>{{ orders.name }}</th>
@@ -42,7 +42,7 @@ export default {
   methods: {
     async getAllOrders() {
     },
-    multiplyPriceTotals(amount, price) {
+    multiplyPriceTotalOrders(amount, price) {
       return (Math.round(amount * price * 100) / 100).toFixed(2);     
     }
   } 
