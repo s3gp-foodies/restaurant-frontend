3dde8dc881194048bae5e86ff011a76d4e19eab9
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-03-15 15:36:35 +0100

    Added styling orders page

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="orders">
-
-    <table v-for="order in data['orders']" :key="order">
-      <tr v-for="product in order['product']" :key="product">
-        <td>{{ product.name }}</td>
-        <td>{{ product.amount }} x {{ product.price }}</td>
-        <td>{{ product.totalprice }}</td>
-      </tr>
-      <tr>
-        <th>{{ order.name }}</th>
-        <th></th>
-        <th>{{ order.totalprice }}</th>
-      </tr>
-    </table>
-
-    <p>Totaal: {{ data['orders'].totalprice }}</p>
-
-    <span>
-      <button>Kassa betalen</button>&nbsp;
-      <button>Digitaal betalen</button>
-    </span>
-
+    <hr size="3" width="85%">  
+    <div class="overview">
+      <table v-for="order in data['orders']" :key="order">
+        <tr v-for="product in order['product']" :key="product">
+          <td>{{ product.name }}</td>
+          <td>{{ product.amount }} x &euro;&thinsp;{{ product.price }}</td>
+          <td>&euro;&thinsp;{{ product.totalprice }}</td>
+        </tr>
+        <tr>
+          <th>{{ order.name }}</th>
+          <th></th>
+          <th>&euro;&thinsp;{{ order.totalprice }}</th>
+        </tr>
+      </table>
+    </div>
+    <hr size="3" width="85%">  
+    <div class="totalprice">
+      <h4>Totaal: &euro;&thinsp;{{ data['orders'].totalprice }}</h4>
+    </div>
+    <div class="payment-buttons">
+      <button class="btn btn-primary">Kassa betalen</button>
+      <button class="btn btn-primary">Digitaal betalen</button>
+    </div>
   </div>
 </template> 
 
@@ -83,19 +85,39 @@ export default {
   flex-direction: column;
   justify-content: center;
 }
+.orders td, .orders th {
+  padding-top: 1px;
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-bottom: 1px;
+}
 .orders td:first-of-type, .orders th:first-of-type {
-  padding-right: 50px;
+  padding-right: 162px;
+  padding-top: 3px;
 }
 .orders td:last-of-type, .orders th:last-of-type {
-  padding-left: 20px;
+  padding-left: 52px;
 }
 .orders td:first-of-type {
   float: left;
 }
 .orders tr {
-  background-color: rgba(160, 248, 219, 0.514);
+  background-color: #bbbbbb8c;
 }
 .orders table {
   padding: 10px;
+}
+.orders table:first-of-type {
+  margin-bottom: 16px;
+}
+.orders .payment-buttons {
+  margin-top: 8px;
+}
+.orders .payment-buttons button:first-of-type {
+  margin-right: 38px;
+}
+.orders .overview {
+  margin-bottom: 6px;
+  margin-top: 6px;
 }
 </style>
