04113e1c7c7feea99643f1b6ea6885378750c9f5
Bas <onrust.bas@gmail.com>
2022-03-21 10:42:56 +0100

    Cleanup orders methods

---
+++ src/temp/ordersMethods.js
@@ -0,0 +1,38 @@
+export default {
+    multiplyPriceTotalProduct(orders) {
+        for (let i = 0; i < orders.length; i++) {
+            for (let j = 0; j < orders[i]['product'].length; j++) {  
+                var orderprice = orders[i]['product'][j].price;
+                var orderamount = orders[i]['product'][j].amount;
+
+                orders[i]['product'][j].totalprice = (orderamount * orderprice).toFixed(2);
+            }
+        }
+
+        return orders;
+    },
+    addPriceTotalOrder(orders) {
+        var totalprice = 0;
+
+        for (let i = 0; i < orders.length; i++) {
+            for (let j = 0; j < orders[i]['product'].length; j++) {  
+            totalprice += parseFloat(orders[i]['product'][j].totalprice); 
+
+            orders[i].totalprice = totalprice.toFixed(2);    
+            }
+        }
+
+        return orders;
+    },
+    addPriceTotalOrders(orders) {
+        var totalprice = 0;
+
+        for (let i = 0; i < orders.length; i++) {
+            totalprice += parseFloat(orders[i].totalprice); 
+        } 
+
+        orders.totalprice = totalprice.toFixed(2);
+
+        return orders;
+    }
+}
