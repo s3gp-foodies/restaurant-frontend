e6cbe37c1e214eb811e9021887aac94661a511cc
Francois Haan <461403@student.fontys.nl>
2022-03-21 09:29:46 +0100

    Order vue commit restaurant frontend

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -3,10 +3,14 @@
     <h2>Order Page</h2>
   </div>
   <p>Menu-item</p>
-  <input v-model="testing" class="form-control" type="number" placeholder={testing} @input="OnInput">
+  <input v-model="this.items.amount" class="form-control" type="number" placeholder=0 @input="OnInput">
   <button type="button" class="btn btn-success" @click="add">+</button>
   <button type="button" class="btn btn-danger" @click="del">-</button>
-  <div v-if="this.testing >9">Cannot be more then 10</div>
+  <div v-if="this.items.amount >14">Cannot be more then 15</div>
+
+    <div v-for="(items) in items" :key="items">
+        {{items}}
+    </div>
 </template> 
 
 <script>
@@ -15,24 +19,31 @@ export default {
   
   methods: {
       add : function () {
-          this.testing ++,
+          this.items.amount ++,
           this.OnInput()
       },
       del : function (){
-          this.testing --,
+          this.items.amount --,
           this.OnInput()
+        console.log(this.$data.items)
       },
       OnInput: function(){
-        if(this.testing  <0){
-          this.testing = 0
-        }else if (this.testing >= 10){
-          this.testing = 10
+        if(this.items.amount  <0){
+          this.items.amount = 0
+        }else if (this.items.amount >= 15){
+          this.items.amount = 15
         }
+        sessionStorage.setItem("amount", this.items.amount);
       }
   },
   data() {
     return {
-      testing:0
+      items: {
+        item1: {
+          name: "test",
+          amount: 0
+        }
+      }
     }
   }
 }
