fe8a9e628cfc0394e74fbb848968079beae25639
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-03-22 11:18:29 +0100

    small fix socket location

--- src/services/websocketConnectionService.js
+++ src/services/websocketConnectionService.js
@@ -3,7 +3,7 @@ import Stomp from 'webstomp-client'
 
 export const websocketConnection = {
     openWebsocketConnection() {
-        this.socket = new SockJS("http://localhost:7209/hubs/table");
+        this.socket = new SockJS("https://localhost:7209/hubs/table");
         this.stompClient = Stomp.over(this.socket);
     }
 }
