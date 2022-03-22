269cde3dc205109d8114f0235c5f47a4bed05771
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-03-22 11:17:22 +0100

    Removed unnecessary methods

--- src/services/websocketConnectionService.js
+++ src/services/websocketConnectionService.js
@@ -5,17 +5,5 @@ export const websocketConnection = {
     openWebsocketConnection() {
         this.socket = new SockJS("http://localhost:7209/hubs/table");
         this.stompClient = Stomp.over(this.socket);
-    },
-    createWebsocketConnection(items, name) {
-        this.stompClient.connect({}, () => {
-            this.stompClient.subscribe("/topic/"+name, payload => {
-                if(payload.body !== null) {
-                    items.push(payload.body);
-                }
-            });
-        });
-    },
-    sendMessageToServer(items, name) {
-        this.stompClient.send("/app/"+name+".input", JSON.stringify({ data: items }), {});
     }
 }
