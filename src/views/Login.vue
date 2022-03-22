496c107e7df2132fac12f345c4128b4afcc4a298
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-03-22 10:35:22 +0100

    Added login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,21 @@
 <template>
+    <hr size="3" width="85%">  
   <div class="login">
-    <h2>Login Page</h2>
+    <div class="panel">
+      <form action="#" @submit.prevent="login">
+        <div class="section">
+          <label for="username">Username:</label>
+          <input required type="text" name="username"/>  
+        </div>
+        <div class="section">
+          <label for="password">Password:</label>
+          <input required type="password" name="password"/>  
+        </div>
+        <div class="section">
+          <button type="login">Login</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template> 
 
@@ -11,4 +26,17 @@ export default {
 </script>
 
 <style scoped>
+.login.panel {
+   margin-top: 300px;
+}
+.login form {
+  display: block;
+  margin: auto;
+  border: 1px solid black;
+  width: 250px;
+  padding: 15px 5px;
+}
+.login .section:not(:first-of-type), .loginpage input {
+  margin-top: 10px;
+}
 </style>
