0dafcabb4dfd9c60e97fe9f5448e03bc4ddcfc1a
Francois Haan <461403@student.fontys.nl>
2022-03-24 06:58:28 +0100

    need to change object and remove duplication

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,20 +1,43 @@
 <template>
-  <div class="order">
-    <h2>Order Page</h2>
-  </div>
-  <p>Menu-item</p>
-  <input v-model="this.items.amount" class="form-control" type="number" placeholder=0 @input="OnInput">
-  <button type="button" class="btn btn-success" @click="add">+</button>
-  <button type="button" class="btn btn-danger" @click="del">-</button>
-  <div v-if="this.items.amount >14">Cannot be more then 15</div>
+<!--  <div class="order">-->
+<!--    <h2>Order Page</h2>-->
+<!--  </div>-->
+<!--  <p>Menu-item</p>-->
+<!--  <input v-model="this.items.amount" class="form-control" type="number" placeholder=0 @input="OnInput">-->
+<!--  <button type="button" class="btn btn-success" @click="add">+</button>-->
+<!--  <button type="button" class="btn btn-danger" @click="del">-</button>-->
+<!--  <div v-if="this.items.amount >14">Cannot be more then 15</div>-->
 
-    <div v-for="(items) in items" :key="items">
-        {{items}}
+<!--    <div v-for="(items) in items" :key="items">-->
+<!--        {{items}}-->
+<!--    </div>-->
+
+<!--  <div v-for="(amount,items) in haha" :key="items">-->
+<!--    {{items}}-->
+<!--  </div>-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <div v-for="(dishes, item) in haha" :key="dishes" >
+    <div v-for="menuItems in haha" :key="menuItems"  class="divider">
+      <p class="dividerBarTxt"> {{item}} </p>
     </div>
-
-  <div v-for="(amount,items) in haha" :key="items">
-    {{items}}
+    <div v-for="(dish, key) in dishes" :key="dish" class="card text-black bg-light mb-3 menuItem">
+      <h3 class="display-6 card-header">{{key}}</h3>
+      <div class="card-body text-black">
+        <img class="foodImage" :src="dish.imgLink" alt="">
+        <p class="foodTxt"> {{dish.description}} </p>
+        <p class="priceTxT">
+          <button type="button" class="btn btn-outline-secondary" @click="subtractfrom(dish)"><i class="fa fa-minus"></i></button>
+          <input style="height: 30px;" type="number" class="numberInput" v-model="dish.amount" @input="OnInput(dish.amount, dish)" />
+          <strong v-if="dish.amount >14">Cannot be more then 15</strong>
+          <strong v-if="dish.amount <0 ">Cannot be less then 0</strong>
+          <button type="button" class="btn btn-outline-secondary" @click="addto(dish)"><i class="fa fa-plus"></i></button>
+          <!--      <button  type="submit" class="btn btn-outline-secondary"> Voeg toe </button> <br>-->
+          &euro; {{dish.price}} Per stuk  </p>
+      </div>
+    </div>
   </div>
+
+
 </template> 
 
 <script>
@@ -22,22 +45,20 @@ export default {
   name: 'OrderPage',
   
   methods: {
-      add : function () {
-          this.items.amount ++
-          this.OnInput()
-      },
-      del : function (){
-          this.items.amount --
-          this.OnInput()
-        console.log(this.$data.items)
-      },
-      OnInput: function(){
-        if(this.items.amount  <0){
-          this.items.amount = 0
-        }else if (this.items.amount >= 15){
-          this.items.amount = 15
-        }
-        sessionStorage.setItem("amount", this.items.amount);
+    addto: function (dish){
+      dish.amount++
+      this.OnInput(dish.amount, dish)
+    },
+    subtractfrom: function (dish){
+      dish.amount--
+      this.OnInput(dish.amount, dish)
+    },
+    OnInput: function (amount, dish){
+      if(dish.amount  <0){
+        dish.amount = 0
+      }else if (dish.amount >= 15){
+        dish.amount = 15
+      }
       }
   },
   data() {
@@ -56,10 +77,48 @@ export default {
   mounted (){
     let haha = JSON.parse(sessionStorage.getItem("Test"))
     this.haha  = haha
+    console.log(haha.dishes)
     console.log(haha)
-}
-}
+},
+  computed : {
+    }
+  }
 </script>
 
 <style scoped>
+
+.dividerBarTxt {
+  margin: 5px;
+}
+
+.divider {
+  padding: 10px;
+  color: #f2f2f2;
+  margin: auto;
+  width: 65%;
+  border: #01192d 1px solid;
+  background-color: #02315a;
+}
+
+.menuItem {
+  display: inline-block;
+  border:  black 1px solid;
+  margin: 10px 10px 10px 30px;
+  padding: 10px;
+  width: 33%;
+}
+
+.foodImage {
+  width: 180px;
+  float: left;
+}
+
+.foodTxt {
+  margin: 5px;
+  padding: 5px;
+}
+
+.priceTxT {
+  font-weight: bold;
+}
 </style>
