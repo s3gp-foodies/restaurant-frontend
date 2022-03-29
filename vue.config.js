255673286a4ac05de64b90917869459af20d6477
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-03-29 11:36:54 +0200

    Changed ip call backend port

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: 'localhost',
-    https: true
+    https: true,
+    host: 'localhost'
   }
 })
