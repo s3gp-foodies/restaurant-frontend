c27e8982f0f56e8259276302ac2aca18e97ef5ee
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-12 13:34:51 +0200

    Update orders

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -28,7 +28,7 @@
 
 <script>
 import orders from '../temp/orders.json'
-import menu from '../temp/backendMenu.json'
+import fullMenu from '../temp/backendMenu.json'
 import { ordersCalculations } from '../helpers/ordersHelper.ts'
 import ProductOrder from '@/models/productorder'
 import Order from '@/models/order'
@@ -39,7 +39,7 @@ export default {
   data: () => {
     return {
       data: orders,
-      menu
+      fullMenu
     }
   },
   props: {
@@ -47,22 +47,42 @@ export default {
   },
   created() {
     // added ordered products 
-    var product_order_1 = new ProductOrder(0, 3); // 3*Salade
+    var product_order_1 = new ProductOrder(5, 3); // 3*Cheesecake
     var product_order_2 = new ProductOrder(1, 4); // 4*Fruit Punch
-    var product_order_3 = new ProductOrder(2, 2); // 2*Zalm Toast
+    var product_order_3 = new ProductOrder(3, 2); // 2*T-Bone Steak
 
     // added orders with ordered products
     var order_1 = new Order(0, '01:10' , [product_order_1, product_order_3]);
     var order_2 = new Order(1, '02:10' , [product_order_2]);
-    console.log(order_1, order_2);
 
-    // find products in menu from ordered products
+    // find ordered products ids and amounts in order1
+    console.log("find ordered products ids and amounts in order1");
+    order_1['products'].forEach((ordered_product) => {
+      console.log(ordered_product.productid + " | " + ordered_product.count);
+    });
+
+    // find ordered products ids and amounts in order2
+    console.log("find ordered products ids and amounts in order2");
+    order_2['products'].forEach((ordered_product) => {
+      console.log(ordered_product.productid + " | " + ordered_product.count);
+    });
+
+    // find ids and names from all products
+    console.log("find ids and names from all products");
+    fullMenu.forEach((product) => {
+      console.log(product.id + " | " + product.name);
+    });
+
+    //find products in menu from ordered products
     
 
     // added menu with products
-    //var menu_1 = new Menu([products_1, products_2]);
-    //console.log(menu_1);
+    //var orderedMenu = new Menu([products_1, products_2]);
+    //console.log(OrderedMenu);
 
+
+
+    //temporary
     this.data['orders'] = ordersCalculations.multiplyPriceTotalProduct(this.data['orders']);
     this.data['orders'] = ordersCalculations.addPriceTotalOrder(this.data['orders']);
     this.data['orders'] = ordersCalculations.addPriceTotalOrders(this.data['orders']); 
