4773bc5e33a6fb1c95500e4613e539625cd0cd17
David Hellinga <d.hellinga@student.fontys.nl>
2022-04-19 20:30:58 +0200

    Refactored the menu to work with services and split it into components.

---
+++ src/components/MenuCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card text-black bg-light mb-3 menuItem">
+    <h3 class="display-6 card-header">{{ name }}</h3>
+    <div class="card-body text-black">
+      <img v-if="photo == null" class="foodImage"
+           src="https://via.placeholder.com/150x150" alt="">
+      <img v-else class="foodImage" :src="photo" alt="">
+      <p class="foodTxt"> {{ description }} </p>
+      <p class="priceTxT">
+        <button type="button" class="btn btn-outline-secondary"><i class="fa fa-minus"></i></button>
+        <input style="height: 35px; width: 50%;" type="number" class="numberInput" min="0"/>
+        <button type="button" class="btn btn-outline-secondary"><i class="fa fa-plus"></i></button>
+        <button type="submit" class="btn btn-outline-secondary"> Voeg toe</button>
+        <br>
+        &euro; {{ price }} Per stuk
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuCard",
+  props: {
+    name: String,
+    photo: String,
+    description: String,
+    price: Number
+  }
+}
+</script>
+
+<style scoped>
+.menuItem {
+  display: inline-block;
+  border: black 1px solid;
+  margin: 10px 10px 10px 30px;
+  width: 33%;
+}
+
+.foodImage {
+  width: 150px;
+  height: 150px;
+  float: left;
+  padding: 10px;
+}
+
+.foodTxt {
+  margin: 5px;
+  padding: 5px;
+}
+
+.priceTxT {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/MenuCategory.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="divider" v-on:click="toggle()">
+    <p class="dividerBarTxt">{{ category.name }}</p>
+  </div>
+  <template v-for="item in categoryItems" :key="item" >
+    <MenuCard
+        v-show="showSection"
+        :name="item.name"
+        :photo="null"
+        :description="item.description"
+        :price="item.price"
+    ></MenuCard>
+  </template>
+</template>
+
+<script>
+import Category from "@/models/category";
+import MenuCard from "@/components/MenuCard";
+
+export default {
+  name: "MenuCategory",
+  components: {MenuCard},
+  props: {
+    category: Category,
+    categoryItems: {}
+  },
+  data: () => {
+    return {
+      showSection: true
+    }
+  },
+  methods: {
+    toggle() {
+      this.showSection = !this.showSection
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.dividerBarTxt {
+  margin: 5px;
+}
+
+.divider {
+  padding: 10px;
+  color: #f2f2f2;
+  margin: auto;
+  width: 65%;
+  border: #01192d 1px solid;
+  background-color: #02315a;
+}
+</style>
