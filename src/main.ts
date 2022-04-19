2217cdf36fea6d00e44e294caacbbbe0d07d7362
David Hellinga <d.hellinga@student.fontys.nl>
2022-04-19 20:27:19 +0200

    fixed imports, cleaned up layout

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import VeeValidate from 'vee-validate';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 import {
-  faHome,
-  faUser,
-  faUserPlus,
-  faSignInAlt,
-  faSignOutAlt
+    faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
