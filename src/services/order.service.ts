c356fb821cc15830e485bacc11418245f5e8eaac
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-05-24 01:26:08 +0200

    Adjusted api call loadorders

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -4,7 +4,8 @@ import OrderProduct from "@/models/order-product";
 import Product from "@/models/product";
 import {useToast} from "vue-toastification";
 import {SocketConsumer} from "@/services/socket-consumer";
-
+import axios from 'axios';
+import authHeader from "@/helpers/auth-header";
 
 const toast = useToast();
 const API_URL = 'https://localhost:7209/api/order/';
@@ -59,17 +60,16 @@ class OrderService extends SocketConsumer {
 
 
     async LoadOrders() {
-        // await axios.get(API_URL+"orders", {headers: authHeader()}).then(response => {
-        //     response.data.forEach((response: any) => {
-        //         sessionOrders.orders.push(<Order>({
-        //             id: response.Id,
-        //             time: response.OrderTime,
-        //             products:
-        //         }));
-        //     });
-        //     }).catch(error => {
-        //     console.log(error)
-        // });
+        /*await axios
+            .get(API_URL + "orders", { headers: authHeader() })
+            .then(response => {
+                console.log("response orders");
+                console.log(response); 
+            })
+            .catch(error => {
+                console.log(error)
+            })*/ 
+
         //TODO: This is a mock
         if (sessionOrders.orders.length === 0) {
             const product_order_1 = new OrderProduct(2, 1);
