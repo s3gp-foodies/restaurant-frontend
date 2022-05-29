744a99297936bbcae59b0e283ceaacc6faf0834a
Francois Haan <461403@student.fontys.nl>
2022-05-29 23:53:40 +0200

    working on delete and checks for creating orders

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -59,6 +59,9 @@ class OrderService extends SocketConsumer {
     }
 
     public MakeOrder(){
+        if(currentOrder.length == 0){
+            console.log("testing")
+        }
         this._socketService?.Invoke("SubmitOrder", currentOrder).then(async () => localStorage.removeItem("AllOrdersOverview")).catch(() => toast.warning("wrong"))
             .then(() => toast.success("Order added"))
         router.push({ path: '/menu'}).then(() =>window.location.reload())
