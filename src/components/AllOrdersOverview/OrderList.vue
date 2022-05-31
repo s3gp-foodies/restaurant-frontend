5bb090f1ac6ee42ac9b2efa1ec477298ad862a8f
Bas <onrust.bas@gmail.com>
2022-05-31 11:13:07 +0200

    Removed unused code

--- src/components/AllOrdersOverview/OrderList.vue
+++ src/components/AllOrdersOverview/OrderList.vue
@@ -59,12 +59,10 @@ export default {
       this.categories = store.state.categories;
       const orders = this.orderService.GetOrders();
 
-      //console.log(orders);
+      this.orderedProducts = []
+      this.totalPrice = 0
 
-      //push productid's and orderedproducts in seperate array's
       orders.forEach((order) => {
-        //console.log(order)
-
         order.products.forEach((orderedProduct) => {
           this.orderedProducts.push(orderedProduct);
         });
@@ -82,7 +80,7 @@ export default {
         });
         if (!set) groupedProducts.push(prod);
       });
-      
+
       groupedProducts.forEach((prod) => {
         const menuProduct = store.getters.GetProductById(prod.productId);
         if (!this.listedCategories.includes(menuProduct.category))
@@ -99,97 +97,9 @@ export default {
         this.totalPrice +=
           this.overview_products[this.overview_products.length - 1].totalPrice;
       });
+
       this.$emit("totalPrice", this.totalPrice);
-      /*
-      this.overview_products = groupedProducts;
-      //console.log(this.orderedProducts);
-
-      //duplicate overview products
-      var duplicate_overview_products = [];
-      var duplicate_product_ids = this.checkDuplicateProductIds(
-        this.productIds
-      );
-
-      duplicate_product_ids.forEach((duplicateProductId) => {
-        var duplicateProductCount = 0;
-        let product = store.getters.GetProductById(duplicateProductId);
-
-        this.orderedProducts.forEach((orderedProduct) => {
-          if (duplicateProductId === orderedProduct.productId) {
-            duplicateProductCount += orderedProduct.count;
-
-            this.totalPrice += product.price * orderedProduct.count;
-          }
-        });
-
-        duplicate_overview_products.push(
-          new OrderOverviewProduct(
-            duplicateProductId,
-            product.name,
-            product.price,
-            duplicateProductCount,
-            product.price * duplicateProductCount
-          )
-        );
-
-        if (!this.listedCategories.includes(product.category))
-          this.listedCategories.push(product.category);
-      });
-
-      //unique overview products
-      var unique_overview_products = [];
-      var unique_product_ids = this.checkUniqueProductIds(
-        duplicate_product_ids
-      );
-
-      unique_product_ids.forEach((uniqueProductId) => {
-        let product = store.getters.GetProductById(uniqueProductId);
-
-        this.orderedProducts.forEach((orderedProduct) => {
-          if (uniqueProductId == orderedProduct.productId) {
-            unique_overview_products.push(
-              new OrderOverviewProduct(
-                uniqueProductId,
-                product.name,
-                product.price,
-                orderedProduct.count,
-                product.price * orderedProduct.count
-              )
-            );
-
-            this.totalPrice += product.price * orderedProduct.count;
-          }
-        });
-
-        if (!this.listedCategories.includes(product.category))
-          this.listedCategories.push(product.category);
-      });
-
-      //combine unique overiew products and duplicate overview products
-      this.overview_products = duplicate_overview_products.concat(
-        unique_overview_products
-      );
-      console.log(this.overview_products);
-
-      //return totalprice
-      this.$emit("totalPrice", this.totalPrice);*/
-    },
-    checkDuplicateProductIds(productIds) {
-      const set = new Set(productIds);
-
-      const duplicates = productIds.filter((item) => {
-        if (set.has(item)) {
-          set.delete(item);
-        } else {
-          return item;
-        }
-      });
-
-      return duplicates;
-    },
-    checkUniqueProductIds(duplicate_product_ids) {
-      return this.productIds.filter((x) => !duplicate_product_ids.includes(x));
-    },
+    }
   },
 };
 </script>
