7e0c20c41b7b2a71e8fa8096820cf606c4f4b7aa
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-06-14 00:00:33 +0200

    small change

--- src/components/LoginLogout.vue
+++ src/components/LoginLogout.vue
@@ -33,9 +33,7 @@ export default {
 
           alert("Logout successful");
         } else {     
-
-          localStorage.loginStatus = 'true'; 
-
+          
           alert("Logout canceled");         
         }
     }
